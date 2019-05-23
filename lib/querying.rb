638910ec3fa9e2ef1f99703413dd2bf97aa1d13b
Chet  Chopra <chopra.chet@gmail.com>
2019-05-23 20:46:24 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,8 +8,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "select characters.species from series
-  join characters on series.id = characters.series_id;"
+  "select count(characters.species) from series
+  join characters on series.id = characters.series_id
+  group by characters.species;"
 end
 
 def select_name_and_series_subgenres_of_authors

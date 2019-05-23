01e35c0c8869890a0d2eba7b64310a29f16294d5
Chet  Chopra <chopra.chet@gmail.com>
2019-05-23 20:36:59 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,9 +1,9 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "select title, year from books where series_id = 1 order by year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "select name, motto from characters where length(motto) = (select max(length(motto)) from characters);"
 end
 
 
@@ -12,7 +12,9 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "select authors.name, subgenres.name from series
+   join authors on series.author_id = authors.id
+   join subgenres on subgenres.id = series.subgenre_id"
 end
 
 def select_series_title_with_most_human_characters

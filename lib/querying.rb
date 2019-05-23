2957560f933fe0a6bb67e05ce20de1e455d0e051
Chet  Chopra <chopra.chet@gmail.com>
2019-05-23 20:45:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,8 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "select series.title from series
+  join characters on series.id = characters.series_id;"
 end
 
 def select_name_and_series_subgenres_of_authors
